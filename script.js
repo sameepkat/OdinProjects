145c77b9777404f5775efb2a488efdce5e34a8d8
sameepkat <sameepsk2@gmail.com>
2024-06-01 17:20:50 +0545

    Working

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector("#container");
+const resetBtn = document.querySelector("#reset-btn");
+const newGrid = document.querySelector("#newGrid");
+
+//Variable declaration
+let currentSize = 16;
+
+newGrid.addEventListener("click", () => {
+  let newSize = parseInt(prompt("Enter a number between 16-100"));
+  if (newSize < 16 || newSize > 100) {
+    alert("Not a valid size. Defaulting to 16");
+    newSize = 16;
+}
+  currentSize = newSize;
+  generateSquares(currentSize);
+});
+
+resetBtn.addEventListener("click", () => {
+  container.innerHTML = "";
+  generateSquares(currentSize);
+});
+
+function generateSquares(squares) {
+  container.innerHTML = "";
+  for (let i = 0; i < squares * squares; i++) {
+    const square = document.createElement("div");
+
+    square.style.height = `${600 / squares}px`;
+    square.style.width = `${600 / squares}px`;
+    square.addEventListener("mouseenter", () => {
+      square.classList.add("hovered");
+    });
+    container.appendChild(square);
+  }
+}
+generateSquares(currentSize);
